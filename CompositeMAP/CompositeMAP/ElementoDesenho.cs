16b171a9f61b91c1254dcf4527bc47a96299b585
gerc <geisianeribeiro8@gmail.com>
2018-04-15 23:53:29 -0300

    Adicionando prática de MAP-Compoite

---
+++ CompositeMAP/CompositeMAP/ElementoDesenho.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCompositeMAP
+{
+    abstract class ElementoDesenho
+    {
+        /// <summary>
+        /// Classe abstrata para disponibilizar funcionalidades
+        /// Componente
+        /// </summary
+
+        protected string _nome;
+
+        //// Construtor
+        //public ElementoDesenho(string nome)
+        //{
+        //    this._nome = nome;
+        //}
+
+        public abstract void Adicionar(ElementoDesenho d);
+        public abstract void Remover(ElementoDesenho d);
+        public abstract void Exibir(int indent);
+
+
+    }
+}
---
+++ CompositeMAP/CompositeMAP/ElementoPrimitivo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCompositeMAP
+{
+    /// <summary>
+    /// A classe Folha
+    /// </summary>
+    class ElementoPrimitivo : ElementoDesenho
+    {
+        // Construtor
+        //
+        // ": base" - especifica qual construtor da classe base 
+        // deve ser chamado 
+        // durante a criação de instâncias da classe derivada.
+        public ElementoPrimitivo(string nome)
+        {
+            //base força a execução da superclasse
+            base._nome = nome;
+        }
+
+        //Sem implementação útil
+        public override void Adicionar(ElementoDesenho c)
+        {
+            Console.WriteLine("Não é possível Adicionar para um Elemento\n\n");
+        }
+
+        //Sem implementação útil
+        public override void Remover(ElementoDesenho c)
+        {
+            Console.WriteLine("Não é possível Remover de um Elemento\n\n");
+        }
+
+        public override void Exibir(int id)
+        {
+            Console.WriteLine(new String('-', id) + " " + _nome);
+        }
+    }
+}
